9d24795e7e99b1a656eb63bee1e710d565cf88e4
chandanavirupakshi <115235556+chandanavirupakshi@users.noreply.github.com>
2023-01-23 18:37:21 +0530

    Update Model.jsx

--- Bootstrap/src/Examples/Model.jsx
+++ Bootstrap/src/Examples/Model.jsx
@@ -1,37 +1,38 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Employee from './Employee';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
-function Model() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+function ModalEx(props) {
+  console.log(props.detail);
   return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
+    <Modal
+      show={props.show}
+      onHide={props.onHide}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.detail.fname}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="image">
+          <img src={props.detail.img} alt="" />
+        </div>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+        <p>
+          <b>Email: </b> {props.detail.email}
+        </p>
+        <p>
+          <b>Ph No: </b> {props.detail.phNo}
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
-export default Model;
+export default ModalEx;
